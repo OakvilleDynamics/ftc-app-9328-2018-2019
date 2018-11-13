884e043d48c206649db44a68512df3e32d53ab7d
Josef Baltz <josef@baltz.net>
2018-11-13 15:47:11 +0000

    v0.1
    Working

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverControl.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/DriverControl.java
@@ -3,13 +3,12 @@ package org.firstinspires.ftc.teamcode;
 import com.qualcomm.robotcore.eventloop.opmode.OpMode;
 import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 import com.qualcomm.robotcore.hardware.DcMotor;
-import com.qualcomm.robotcore.hardware.DcMotorSimple;
 import com.qualcomm.robotcore.util.ElapsedTime;
 
 @TeleOp(name="Driver Control", group="Driver")
 public class DriverControl extends OpMode {
     private ElapsedTime runtime = new ElapsedTime();
-    private DcMotor leftDrive, rightDrive, rightLift, leftLift;
+    private DcMotor leftDrive, rightDrive, lift;
 
     @Override
     public void init() {
@@ -17,13 +16,11 @@ public class DriverControl extends OpMode {
 
         leftDrive = hardwareMap.get(DcMotor.class, "leftdrive");
         rightDrive = hardwareMap.get(DcMotor.class, "rightdrive");
-        leftLift = hardwareMap.get(DcMotor.class, "leftlift");
-        rightLift = hardwareMap.get(DcMotor.class, "rightlift");
+        lift = hardwareMap.get(DcMotor.class, "lift");
 
         leftDrive.setDirection(DcMotor.Direction.FORWARD);
         rightDrive.setDirection(DcMotor.Direction.REVERSE);
-        leftLift.setDirection(DcMotor.Direction.FORWARD);
-        rightLift.setDirection(DcMotor.Direction.REVERSE);
+        lift.setDirection(DcMotor.Direction.FORWARD);
     }
 
     @Override
@@ -41,23 +38,22 @@ public class DriverControl extends OpMode {
 
         leftDrive.setPower(leftDrivePower);
         rightDrive.setPower(rightDrivePower);
-        if (gamepad1.dpad_up) {
-            leftLift.setPower(0.25);
-            rightLift.setPower(0.25);
+
+        if (gamepad1.dpad_up && gamepad1.a) {
+            lift.setPower(1);
+        } else if (gamepad1.dpad_down && gamepad1.a) {
+            lift.setPower(-1);
+        } else if (gamepad1.dpad_up) {
+            lift.setPower(0.2);
         } else if (gamepad1.dpad_down) {
-            leftLift.setPower(-0.25);
-            rightLift.setPower(-0.25);
-        } else {
-            leftLift.setPower(0);
-            rightLift.setPower(0);
-        }
+            lift.setPower(-0.2);
+        } else { lift.setPower(0); }
     }
 
     @Override
     public void stop() {
         leftDrive.setPower(0);
         rightDrive.setPower(0);
-        leftLift.setPower(0);
-        rightLift.setPower(0);
+        lift.setPower(0);
     }
 }
